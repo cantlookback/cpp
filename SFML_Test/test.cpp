f1e0d6644d369c7332f9d30dbec2a919026105c1
cantlookback <zinakov22@gmail.com>
2021-11-21 19:17:05 +0700

    main

--- SFML_Test/test.cpp
+++ SFML_Test/test.cpp
@@ -5,12 +5,6 @@ using namespace std;
 
 int main(){
 
-    int a = 5;
-    string mem = "amogus";
-    cout << mem + " blya";
-    int b = 10;
-    int c = 1;
-    cout << a + b << endl;
-    cout << "beb" << endl;
+    
     return 0;
 }
