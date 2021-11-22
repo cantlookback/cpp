cfe34cab92bf5e490ffd3d3bebebeed9da82737a
cantlookback <zinakov22@gmail.com>
2021-11-23 03:24:12 +0700

    new dir for some utils

---
+++ Utilities/utilities.cpp
@@ -0,0 +1,8 @@
+#include "utilities.h"
+
+void clearScreen(){	
+    COORD cursorPosition;
+    cursorPosition.X = 0;
+    cursorPosition.Y = 0;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+}
---
+++ Utilities/utilities.h
@@ -0,0 +1,10 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void clearScreen();
+
+#endif
